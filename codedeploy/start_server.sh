602d9c0f3370d88f5eb10b961ce555c6b3795740
Quang dung Nguyen <quangdungnguyen@MacBook-Pro-cua-Quang.local>
2022-06-27 22:50:37 +0700

    update 4

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/ec2-user/web/.env
+#source /home/ec2-user/web/.env
 
 $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
 cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
